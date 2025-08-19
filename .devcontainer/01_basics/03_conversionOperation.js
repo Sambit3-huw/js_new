356f20848c2283f546fefc8cf8d3794517aa8410
Sambit3-huw <sambitkumarpaikaray660@gmail.com>
2025-08-19 02:42:19 +0000

    Conversion

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,24 +1,48 @@
-let score = "undefined"
-console.log(typeof score)
-console.log(typeof(score))
+//let score = "undefined"
+//console.log(typeof score)
+//console.log(typeof(score))
 
-let valueInNUmber = Number(score)
-console.log(typeof valueInNumber);
+//let valueInNUmber = Number(score)
+//console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
 //"33"=33
 //"33abc"=>Nan
 // true= 1; false = 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0=>false
 //"" => false
 //"Sambit"=> true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+//*************************Operations **************
+
+//let value = 3
+//let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+
+let str1 = "hello"
+let str2 = " Sambit"
+
+let str3 =str1+str2
+console.log(str3)
+
+console.log("1"+ 2)
+console.log(1 + "2")
+console.log("1" + "2")// string comes first = output is string
+console.log(1 + 2 + "3")//string comes last then 1+2 after "3"= 33
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter) 
+//https://tc39.es/ecma262/#sec-addition-operator-plus to study
